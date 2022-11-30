4dc69db4aaff7970c9feae06cbef4b09a759ad86
«Artur» <art13471118@gmail.com>
2022-11-30 19:29:10 +0300

    Добавил команду Random

--- task4_dividing_numbers/Program.cs
+++ task4_dividing_numbers/Program.cs
@@ -1,3 +1,5 @@
-﻿double numbera = 13;
-double numberb = 5;
+﻿double numbera = new Random().Next(1,10); // 1,2,3.....9
+Console.WriteLine (numbera);
+double numberb = new Random().Next(1,10);
+Console.WriteLine (numberb);
 Console.WriteLine(numbera / numberb);
