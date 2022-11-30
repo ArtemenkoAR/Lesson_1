5138f4ef0a4c2e000bdf47e902bcf3d1b187ccaa
«Artur» <art13471118@gmail.com>
2022-11-30 20:27:05 +0300

    Добавил папку task_favorite

---
+++ task_favorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "art")
+{
+    Console.WriteLine("Привет, my dear friend");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
